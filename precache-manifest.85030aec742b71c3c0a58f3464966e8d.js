73f5e2ec734328c0e80f373f54e1a30f9e2d18d4
lihereshma <reshmalihe00068@gmail.com>
2020-09-09 19:11:28 +0530

    Updates

---
+++ precache-manifest.85030aec742b71c3c0a58f3464966e8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c941d43a14e14bbeb39e4bef983d1ff",
+    "url": "/credit-card-react/index.html"
+  },
+  {
+    "revision": "4f835f4eb6260237803f",
+    "url": "/credit-card-react/static/css/2.f59b35b6.chunk.css"
+  },
+  {
+    "revision": "3c0eaa9c9ddfba44add2",
+    "url": "/credit-card-react/static/css/main.5f099509.chunk.css"
+  },
+  {
+    "revision": "4f835f4eb6260237803f",
+    "url": "/credit-card-react/static/js/2.7b44df88.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/credit-card-react/static/js/2.7b44df88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0eaa9c9ddfba44add2",
+    "url": "/credit-card-react/static/js/main.4b2fcdb6.chunk.js"
+  },
+  {
+    "revision": "77c6914127c6e5dd3973",
+    "url": "/credit-card-react/static/js/runtime-main.fd1e49d7.js"
+  }
+]);
